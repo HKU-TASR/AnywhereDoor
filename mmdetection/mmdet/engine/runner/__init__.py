bea1c58ef40b00f8464aa0268f76c24b8f9bb98a
Jialin Lu <72877752+hku-jialin@users.noreply.github.com>
2025-03-11 14:11:29 +0800

    Delete mmdetection directory

--- mmdetection/mmdet/engine/runner/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .loops import TeacherStudentValLoop
-from .backdoor_loops import BackdoorValLoop
-
-__all__ = ['TeacherStudentValLoop', 'BackdoorValLoop']
